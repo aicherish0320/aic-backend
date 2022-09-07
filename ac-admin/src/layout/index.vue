f247bb7b53b98b291edcf054428ac163329a3d51
aicherish0320 <aicherish0320@163.com>
2022-09-07 09:47:27 +0800

    创建基于 Layout 的基础架构

--- ac-admin/src/layout/index.vue
+++ ac-admin/src/layout/index.vue
@@ -1,7 +1,43 @@
 <template>
-  <div>layout</div>
+  <div class="app-wrapper">
+    <Sidebar
+      class="sidebar-container"
+      :style="{ backgroundColor: variables.menuBg }"
+    ></Sidebar>
+    <div class="main-container">
+      <div class="fixed-header">
+        <Navbar></Navbar>
+      </div>
+      <AppMain></AppMain>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import Navbar from './components/Navbar.vue'
+import Sidebar from './components/sidebar/index.vue'
+import AppMain from './components/AppMain.vue'
+import variables from '@/styles/variables.module.scss'
 
-<style lang="scss" scoped></style>
+console.log('variables >>> ', variables)
+</script>
+
+<style lang="scss" scoped>
+@import '~@/styles/mixin.scss';
+@import '~@/styles/variables.module.scss';
+
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+
+.fixed-header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9;
+  width: calc(100% - #{$sideBarWidth});
+}
+</style>
