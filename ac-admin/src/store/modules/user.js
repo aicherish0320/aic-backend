97cffdb11628891cac799781fde3e6ba2d0d3878
aicherish0320 <aicherish0320@163.com>
2022-09-07 13:37:15 +0800

    退出登陆方案实现

--- ac-admin/src/store/modules/user.js
+++ ac-admin/src/store/modules/user.js
@@ -1,6 +1,6 @@
 import md5 from 'md5'
 import { login, getUserInfo } from '@/api/sys'
-import { getItem, setItem } from '@/utils/storage'
+import { getItem, removeAllItem, setItem } from '@/utils/storage'
 import { TOKEN } from '@/constant/index'
 import router from '@/router'
 
@@ -41,6 +41,13 @@ export default {
       const ret = await getUserInfo()
       commit('setUserInfo', ret)
       return ret
+    },
+    logout({ commit }) {
+      commit('setToken', '')
+      commit('setUserInfo', {})
+      removeAllItem()
+      // TODO：清除掉权限相关配置
+      router.push('/login')
     }
   }
 }
