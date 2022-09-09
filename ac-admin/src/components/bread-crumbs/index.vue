45a84e139d034916068c7a350ea6c6b059927574
aicherish0320 <aicherish0320@163.com>
2022-09-09 14:20:56 +0800

    依据动态数据，渲染面包屑

--- ac-admin/src/components/bread-crumbs/index.vue
+++ ac-admin/src/components/bread-crumbs/index.vue
@@ -1,19 +1,25 @@
 <template>
   <el-breadcrumb class="breadcrumb" separator="/">
-    <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
     <el-breadcrumb-item
-      ><a href="/">promotion management</a></el-breadcrumb-item
+      v-for="(item, index) in breadcrumbData"
+      :key="item.path"
     >
-    <el-breadcrumb-item>promotion list</el-breadcrumb-item>
-    <el-breadcrumb-item>
-      <span class="no-direct">promotion detail</span>
+      <!-- 不可点击 -->
+      <span v-if="index === breadcrumbData.length - 1" class="no-direct">{{
+        item.meta.title
+      }}</span>
+      <!-- 可点击 -->
+      <span v-else class="direct" @click="onLinkClick(item)">{{
+        item.meta.title
+      }}</span>
     </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
 <script setup>
 import { ref, watch } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
+import { useStore } from 'vuex'
 
 // 生成数组数据
 const breadcrumbData = ref([])
@@ -32,6 +38,15 @@ watch(
     immediate: true
   }
 )
+
+const router = useRouter()
+const onLinkClick = (item) => {
+  router.push(item.path)
+}
+
+// 将来需要主题替换，所以 hover 的颜色我们设置为 主色
+const store = useStore()
+const linkHoverColor = ref(store.getters.cssVar.menuBg)
 </script>
 
 <style lang="scss" scoped>
@@ -40,6 +55,14 @@ watch(
   font-size: 14px;
   line-height: 50px;
   margin-left: 8px;
+  .direct {
+    color: #666;
+    font-weight: 600;
+    cursor: pointer;
+    &:hover {
+      color: v-bind(linkHoverColor);
+    }
+  }
   .no-direct {
     color: #97a8be;
     cursor: text;
