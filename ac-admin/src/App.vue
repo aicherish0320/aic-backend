e1164d3bdc22d7bfdd83aecc075e5621972ecfe0
aicherish <aicherish0320@163.com>
2022-09-12 16:38:18 +0800

    element-ui 新主题立即生效

--- ac-admin/src/App.vue
+++ ac-admin/src/App.vue
@@ -8,8 +8,13 @@ import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 import en from 'element-plus/lib/locale/lang/en'
 import { computed } from 'vue'
 import { useStore } from 'vuex'
+import { generateNewStyle, writeNewStyle } from './utils/theme'
 
 const store = useStore()
 const locale = computed(() => (store.getters.language === 'zh' ? zhCn : en))
+
+generateNewStyle(store.getters.mainColor).then((newStyle) => {
+  writeNewStyle(newStyle)
+})
 </script>
 <style lang="scss"></style>
