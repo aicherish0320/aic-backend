ae0911874a2edec091f7a8a5cb0c3925490ee2af
aicherish0320 <aicherish0320@163.com>
2022-09-13 10:02:30 +0800

    章节模块开发

--- ac-admin/src/views/profile/components/Chapter.vue
+++ ac-admin/src/views/profile/components/Chapter.vue
@@ -1,7 +1,31 @@
 <template>
-  <div></div>
+  <el-timeline>
+    <el-timeline-item
+      v-for="item in chapterData"
+      :key="item.id"
+      :timestamp="item.timestamp"
+      placement="top"
+    >
+      <el-card>
+        <h4>{{ item.content }}</h4>
+      </el-card>
+    </el-timeline-item>
+  </el-timeline>
 </template>
 
-<script setup></script>
+<script setup>
+import { chapter } from '@/api/user'
+import { watchSwitchLang } from '@/utils/i18n'
+import { ref } from 'vue'
+
+const chapterData = ref([])
+const getChapterData = async () => {
+  chapterData.value = await chapter()
+}
+
+getChapterData()
+
+watchSwitchLang(getChapterData)
+</script>
 
 <style lang="scss" scoped></style>
