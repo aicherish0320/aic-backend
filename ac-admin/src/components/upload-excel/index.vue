768f6fb47559878845548b1af1c2e96025067e5e
aicherish0320 <aicherish0320@163.com>
2022-09-13 17:53:41 +0800

    提供两种文件导入形式

--- ac-admin/src/components/upload-excel/index.vue
+++ ac-admin/src/components/upload-excel/index.vue
@@ -1,13 +1,70 @@
 <template>
-  <div class="upload-excel"></div>
+  <div class="upload-excel">
+    <div class="btn-upload">
+      <el-button :loading="loading" type="primary" @click="handleUpload">
+        {{ $t('msg.uploadExcel.upload') }}</el-button
+      >
+    </div>
+
+    <input
+      type="file"
+      ref="excelUploadInput"
+      class="excel-upload-input"
+      accept=".xlsx,.xls"
+      @change="handleChange"
+    />
+
+    <div
+      class="drop"
+      @drop.stop.prevent="handleDrop"
+      @dragover.stop.prevent="handleDragover"
+      @dragenter.stop.prevent="handleDragover"
+    >
+      <i class="el-icon-upload" />
+      <span>{{ $t('msg.uploadExcel.drop') }}</span>
+    </div>
+  </div>
 </template>
 
 <script setup>
-// import { ref } from 'vue'
+import { ref } from 'vue'
 
-// const loading = ref(false)
+const loading = ref(false)
 
-// const handleUpload = () => {}
+const handleUpload = () => {}
+const handleChange = () => {}
+
+const handleDrop = () => {}
+const handleDragover = () => {}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.upload-excel {
+  display: flex;
+  justify-content: center;
+  margin-top: 100px;
+  .excel-upload-input {
+    display: none;
+    z-index: -9999;
+  }
+  .btn-upload,
+  .drop {
+    border: 1px dashed #bbb;
+    width: 350px;
+    height: 160px;
+    text-align: center;
+    line-height: 160px;
+  }
+  .drop {
+    line-height: 60px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    color: #bbb;
+    i {
+      font-size: 60px;
+      display: block;
+    }
+  }
+}
+</style>
