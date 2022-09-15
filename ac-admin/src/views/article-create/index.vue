e25c55f9804eb71d02ab08aca55e21ce1f859ab2
aicherish <aicherish0320@163.com>
2022-09-15 23:57:22 +0800

    新建文章结构

--- ac-admin/src/views/article-create/index.vue
+++ ac-admin/src/views/article-create/index.vue
@@ -1,7 +1,51 @@
 <template>
-  <div>创建文章</div>
+  <div class="article-create">
+    <el-card>
+      <el-input
+        class="title-input"
+        :placeholder="$t('msg.article.titlePlaceholder')"
+        v-model="title"
+        maxlength="20"
+        clearable
+      ></el-input>
+      <el-tabs v-model="activeName">
+        <el-tab-pane :label="$t('msg.article.markdown')" name="markdown"
+          >1</el-tab-pane
+        ><el-tab-pane :label="$t('msg.article.richText')" name="editor"
+          >2</el-tab-pane
+        ></el-tabs
+      >
+    </el-card>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { articleDetail } from '@/api/article'
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
 
-<style lang="scss" scoped></style>
+const activeName = ref('markdown')
+const title = ref('')
+
+// const onSuccess = () => {
+//   title.value = ''
+// }
+
+const route = useRoute()
+const articleId = route.params.id
+const detail = ref({})
+const getArticleDetail = async () => {
+  detail.value = await articleDetail(articleId)
+  title.value = detail.value.title
+}
+
+if (articleId) {
+  getArticleDetail()
+}
+</script>
+
+<style lang="scss" scoped>
+.title-input {
+  margin-bottom: 20px;
+}
+</style>
